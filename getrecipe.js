420ff62a0fcc939935b92cf12b5b0b6c1aed8a24
Rushinomi <rushinomib@gmail.com>
2026-01-30 22:56:12 +0530

    utility added

--- getrecipe.js
+++ getrecipe.js
@@ -1,19 +1,59 @@
-import { createElement } from "react"
-
 export const getrecipecard = ( recipes, parentElement, createElement) =>{
+    console.log(recipes);
     for(let recipe of recipes){
         //parent
         const cardContainer = createElement("div");
-        cardContainer.classlist.add("card", "shadow");
+        cardContainer.classList.add("card", "shadow");
 //image cont
         const cardimagecontainer = createElement("div");
-        cardimagecontainer.classlist.add("card-image-container");
+        cardimagecontainer.classList.add("card-image-container");
 
   //card image
 
 const imageelement = createElement("img");
-imageelement.classlist.add("card-image");
-imageelement.setAttribute("src", recipe["image-url"]);
-imageelement.setAttribute("alt", recipe.TranslatedRecipeName)
+    imageelement.classList.add("card-image");
+    imageelement.setAttribute("src", recipe.strMealThumb);
+    imageelement.setAttribute("alt", recipe.strMeal);
+    imageelement.setAttribute("data-id", recipe.id);
+    cardimagecontainer.appendChild(imageelement);
+    cardContainer.appendChild(cardimagecontainer);
+
+
+    //card details container
+    const carddetailscontainer = createElement("div");
+    carddetailscontainer.classList.add("card-details");
+
+    //card title
+    const cardtitleElement = createElement("div");
+    cardtitleElement.classList.add("card-title");
+    cardtitleElement.innerText = recipe.strMeal;
+    carddetailscontainer.appendChild(cardtitleElement);
+
+    //card rating and duration container
+    const cardratingdurationcontainer = createElement("div");
+    cardratingdurationcontainer.classList.add("card-rating");
+
+    //rating element
+    const ratingElement = createElement("div");
+    const ratingValueElement = createElement("span");
+    ratingValueElement.innerText = `Area: ${recipe.strArea}`;
+    ratingElement.appendChild(ratingValueElement);
+    cardratingdurationcontainer.appendChild(ratingElement);
+  
+    //duration
+    const lengthElement = createElement("div");
+    lengthElement.classList.add("star-rating");
+    const ratingiconelement = createElement("span");
+    ratingiconelement.classList.add("time", "material-symbols-outlined");
+    ratingiconelement.innerText = "timer";
+    lengthElement.appendChild(ratingiconelement);
+    const duration = createElement("span");
+    duration.innerText = `ID: ${recipe.idMeal}`;
+    lengthElement.appendChild(duration);
+    cardratingdurationcontainer.appendChild(lengthElement);
+    carddetailscontainer.appendChild(cardratingdurationcontainer);
+    cardContainer.appendChild(carddetailscontainer);
+    parentElement.appendChild(cardContainer);
     }
-}
+};
+
--- rec.js
+++ rec.js
@@ -1,13 +1,20 @@
+import { getrecipecard } from "./getrecipe.js";
+
+const cardparentcontainer = document.querySelector(".main");
 const URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+const createElement = (element) => document.createElement(element);
 const getrecipes = async () => {
     try {
         const {data} = await axios.get(URL);
-        return data;
+        return data.meals;
     }
     catch (error) {
         console.log(error);
     }
 };
- 
+
     const recipes = await getrecipes(URL);
+
+    getrecipecard(recipes, cardparentcontainer, createElement);
     console.log(recipes); 
